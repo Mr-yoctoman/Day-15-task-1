e08dc96c5ee9052c313ab284fb89507df251a09c
Mr.Ashu <145025838+Mr-yoctoman@users.noreply.github.com>
2024-02-27 14:45:07 +0530

    calculator
    Calculator with basic arithmatic functions.

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+function appendCharacter(character) {
+  document.getElementById("display").value += character;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    document.getElementById("display").value = eval(
+      document.getElementById("display").value
+    );
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
